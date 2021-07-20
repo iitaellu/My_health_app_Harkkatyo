5fb40df5dbaa3a7203bf0dcfc38c91f9b9fd0295
iitaellu <78799868+iitaellu@users.noreply.github.com>
2021-07-20 17:58:11 +0300

    Update WaterFragment.java

--- WaterFragment.java
+++ WaterFragment.java
@@ -6,16 +6,59 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class WaterFragment extends Fragment {
+public class WaterFragment extends Fragment implements View.OnClickListener {
+
+    Button smallGlass, mediumGlass, bigGlass, largeGlass;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_water, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_water, container, false);
+
+        Button smallGlass = (Button) rootView.findViewById(R.id.smallGlass);
+        Button mediumGlass = (Button) rootView.findViewById(R.id.mediumGlass);
+        Button bigGlass = (Button) rootView.findViewById(R.id.bigGlass);
+        Button largeGlass = (Button) rootView.findViewById(R.id.largeGlass);
+
+        smallGlass.setOnClickListener(this);
+        mediumGlass.setOnClickListener(this);
+        bigGlass.setOnClickListener(this);
+        largeGlass.setOnClickListener(this);
+
+
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+            case R.id.smallGlass:
+                //add 250ml
+                Toast.makeText(getActivity(),"250ml drunk",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.mediumGlass:
+                //add 500ml
+                Toast.makeText(getActivity(),"500ml drunk",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.bigGlass:
+                //add 600ml
+                Toast.makeText(getActivity(),"600ml drunk",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.largeGlass:
+                //add 1000ml
+                Toast.makeText(getActivity(),"1l drunk",Toast.LENGTH_LONG).show();
+                break;
+        }
+
     }
 
     //TODO käyttäjän antamien tietojen perusteella arvioidaan montako lasillista (250ml), käyttäjän on juotava päivässä.
