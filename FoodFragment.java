b9f1dc43bef6ab2213974d3361af04462e4e5f38
iitaellu <78799868+iitaellu@users.noreply.github.com>
2021-07-21 16:04:32 +0300

    Update FoodFragment.java

--- FoodFragment.java
+++ FoodFragment.java
@@ -10,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -17,6 +18,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FoodFragment extends Fragment implements View.OnClickListener {
+
+    TextView textTotal;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -37,6 +41,8 @@ public class FoodFragment extends Fragment implements View.OnClickListener {
         preferAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerPrefer.setAdapter(preferAdapter);
 
+        textTotal = (TextView) rootView.findViewById(R.id.textTotal);
+
         Button calculateButton = (Button) rootView.findViewById(R.id.calculateButton);
         calculateButton.setOnClickListener(this);
 
@@ -45,6 +51,8 @@ public class FoodFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+
+        textTotal.setText("This is result");
         Toast.makeText(getActivity(),"Calculated and saved",Toast.LENGTH_LONG).show();
 
         //TODO kerää annetut tiedot parametreihin. Näiden parametrien avulla rakennetaan linkki,
@@ -53,3 +61,4 @@ public class FoodFragment extends Fragment implements View.OnClickListener {
         // faktaa https://www.nutrilett.fi/paivittaiset-kalorit-ja-kaloreiden-kulutus/
     }
 }
+
