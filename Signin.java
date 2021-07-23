466fbad30f0a773aab9c5bf2d28726852e910499
Viiuska <87257685+Viiuska@users.noreply.github.com>
2021-07-23 11:24:32 +0300

    Create Signin.java

---
+++ Signin.java
@@ -0,0 +1,117 @@
+package com.example.loginsystem;
+
+//source:
+//https://www.youtube.com/watch?v=tbh9YaWPKKs
+//https://www.youtube.com/watch?v=TwHmrZxiPA8
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Signin extends AppCompatActivity {
+
+    EditText username, email, password;
+    Button sign;
+    TextView login;
+
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        username = findViewById(R.id.etUser);
+        email = findViewById(R.id.etEmail);
+        password = findViewById(R.id.etPassword);
+        sign = findViewById(R.id.btnSignin);
+        login = findViewById(R.id.tvSignin);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Signin.this, Login.class));
+            }
+        });
+
+
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+      
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = email.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+                String name = username.getText().toString().trim();
+                String pattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=]).{8,}";
+
+                if(TextUtils.isEmpty(name)){
+                    username.setError("Username is required");
+                }
+                if(TextUtils.isEmpty(mail)){
+                    email.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(pass)){
+                    password.setError("Password is required");
+                    return;
+                }
+                //Should check the password is good
+                if(pass.matches(pattern)) {
+                    //This shoud check if the password is valid or not, but it only check for the length
+                    password.setError("Password must have at least: 8 character, one uppercase, one number and one symbol");
+                    System.out.println(pass.matches(pattern));
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+
+                //Sign in user in farebase
+                fAuth.createUserWithEmailAndPassword(mail, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Signin.this, "User created", Toast.LENGTH_SHORT).show();
+                            //Home screen in here
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }else{
+                            Toast.makeText(Signin.this, "Error "+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+
+
+    }
+
+
+}
