5d0cf4d1077e965605aa7f84de1783141210ffb9
iitaellu <78799868+iitaellu@users.noreply.github.com>
2021-07-24 16:16:20 +0300

    Update FoodFragment.java

--- FoodFragment.java
+++ FoodFragment.java
@@ -3,6 +3,8 @@
 package com.example.harjoitustyo_ida_viia;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,24 +19,43 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 public class FoodFragment extends Fragment implements View.OnClickListener {
 
     TextView textTotal;
+    EditText beefLevel, fishLevel, pork_PoultryLevel, dairyLevel, cheeseLevel, riceLevel, eggLevel, saladLevel, restaurantLevel;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_food, container, false);
 
-        EditText beefLevel = (EditText) rootView.findViewById(R.id.beefLevel);
-        EditText fishLevel = (EditText) rootView.findViewById(R.id.fishLevel);
-        EditText pork_PoultryLevel = (EditText) rootView.findViewById(R.id.pork_PoultryLevel);
-        EditText dairyLevel = (EditText) rootView.findViewById(R.id.dairyLevel);
-        EditText cheeseLevel = (EditText) rootView.findViewById(R.id.cheeseLevel);
-        EditText riceLevel = (EditText) rootView.findViewById(R.id.riceLevel);
-        EditText eggLevel = (EditText) rootView.findViewById(R.id.eggLevel);
-        EditText saladLevel = (EditText) rootView.findViewById(R.id.saladLevel);
-        EditText restaurantLevel = (EditText) rootView.findViewById(R.id.restaurantLevel);
+        beefLevel = (EditText) rootView.findViewById(R.id.beefLevel);
+        fishLevel = (EditText) rootView.findViewById(R.id.fishLevel);
+        pork_PoultryLevel = (EditText) rootView.findViewById(R.id.pork_PoultryLevel);
+        dairyLevel = (EditText) rootView.findViewById(R.id.dairyLevel);
+        cheeseLevel = (EditText) rootView.findViewById(R.id.cheeseLevel);
+        riceLevel = (EditText) rootView.findViewById(R.id.riceLevel);
+        eggLevel = (EditText) rootView.findViewById(R.id.eggLevel);
+        saladLevel = (EditText) rootView.findViewById(R.id.saladLevel);
+        restaurantLevel = (EditText) rootView.findViewById(R.id.restaurantLevel);
 
         Spinner spinnerPrefer = (Spinner) rootView.findViewById(R.id.spinnerPrefer);
         ArrayAdapter<CharSequence> preferAdapter = ArrayAdapter.createFromResource(getActivity(),R.array.Boolean, android.R.layout.simple_spinner_item);
@@ -42,6 +63,7 @@ public class FoodFragment extends Fragment implements View.OnClickListener {
         spinnerPrefer.setAdapter(preferAdapter);
 
         textTotal = (TextView) rootView.findViewById(R.id.textTotal);
+
 
         Button calculateButton = (Button) rootView.findViewById(R.id.calculateButton);
         calculateButton.setOnClickListener(this);
@@ -52,7 +74,58 @@ public class FoodFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
-        textTotal.setText("This is result");
+        String tempBeef = beefLevel.getText().toString();
+        String tempFish = fishLevel.getText().toString();
+        String tempPork_Poultry = pork_PoultryLevel.getText().toString();
+        String tempDairy = dairyLevel.getText().toString();
+        String tempCheese = cheeseLevel.getText().toString();
+        String tempRice = riceLevel.getText().toString();
+        String tempEgg = eggLevel.getText().toString();
+        String tempSalad = saladLevel.getText().toString();
+        String tempRestaurant = restaurantLevel.getText().toString();
+
+        int beef = 0;
+        int fish = 0;
+        int pork_poultry = 0;
+        int dairy = 0;
+        int cheese = 0;
+        int rice = 0;
+        int egg = 0;
+        int salad = 0;
+        int restaurant = 0;
+        String total = null;
+
+        if(!"".equals(tempBeef)){
+            beef=Integer.parseInt(tempBeef);
+        }
+        if(!"".equals(tempFish)){
+            fish=Integer.parseInt(tempFish);
+        }
+        if(!"".equals(tempPork_Poultry)){
+            pork_poultry=Integer.parseInt(tempPork_Poultry);
+        }
+        if(!"".equals(tempDairy)){
+            dairy=Integer.parseInt(tempDairy);
+        }
+        if(!"".equals(tempCheese)){
+            cheese=Integer.parseInt(tempCheese);
+        }
+        if(!"".equals(tempRice)){
+            rice=Integer.parseInt(tempRice);
+        }
+        if(!"".equals(tempEgg)){
+            egg=Integer.parseInt(tempEgg);
+        }
+        if(!"".equals(tempSalad)){
+            salad=Integer.parseInt(tempSalad);
+        }
+        if(!"".equals(tempRestaurant)){
+            restaurant=Integer.parseInt(tempRestaurant);
+        }
+
+        //total = beef;
+
+        textTotal.setText(total);
         Toast.makeText(getActivity(),"Calculated and saved",Toast.LENGTH_LONG).show();
 
         //TODO kerää annetut tiedot parametreihin. Näiden parametrien avulla rakennetaan linkki,
@@ -61,4 +134,5 @@ public class FoodFragment extends Fragment implements View.OnClickListener {
         // faktaa https://www.nutrilett.fi/paivittaiset-kalorit-ja-kaloreiden-kulutus/
     }
 }
+
 
