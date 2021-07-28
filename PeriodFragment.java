e149ec46157735620002d563863cc10d9a47a7de
Viiuska <87257685+Viiuska@users.noreply.github.com>
2021-07-28 12:12:24 +0300

    Update PeriodFragment.java

--- PeriodFragment.java
+++ PeriodFragment.java
@@ -1,24 +1,20 @@
 package com.example.myhealth;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.My_Health.R;
 
-import java.util.ArrayList;
+
 
 public class PeriodFragment extends Fragment {
 
