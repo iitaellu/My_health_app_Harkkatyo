e555ee321f7731bbd8677ad14adcd5820a7e23f8
Viiuska <87257685+Viiuska@users.noreply.github.com>
2021-07-29 22:36:52 +0300

    Update Signin.java

--- Signin.java
+++ Signin.java
@@ -1,14 +1,17 @@
 package com.example.harjoitustyo_ida_viia;
 
+//https://www.youtube.com/watch?v=RiHGwJ_u27k&list=PLlGT4GXi8_8dDK5Y3KCxuKAPpil9V49rN&index=3&t=2s
+//https://www.youtube.com/playlist?list=PLlGT4GXi8_8dDK5Y3KCxuKAPpil9V49rN
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.TextUtils;
-import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,9 +20,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
 
 public class Signin extends AppCompatActivity {
 
@@ -29,6 +43,8 @@ public class Signin extends AppCompatActivity {
 
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +58,9 @@ public class Signin extends AppCompatActivity {
         login = findViewById(R.id.tvOldUser);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         progressBar = findViewById(R.id.progressBar);
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,13 +70,15 @@ public class Signin extends AppCompatActivity {
         });
 
 
-
+        /*
         if(fAuth.getCurrentUser() != null){
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            startActivity(new Intent(getApplicationContext(), Login.class));
+            Toast.makeText(this, "What happened???", Toast.LENGTH_SHORT).show();
             finish();
         }
+        */
 
-      
+
         sign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,7 +116,17 @@ public class Signin extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(Signin.this, "User created", Toast.LENGTH_SHORT).show();
-                            //Home screen in here
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String, Object> user= new HashMap<>();
+                            user.put("Username", name);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "onSuccess: user Profile is created for"+userID);
+                                }
+                            });
+                            //vaihettu mainactivity testiprofileksi
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         }else{
                             Toast.makeText(Signin.this, "Error "+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
@@ -106,9 +136,6 @@ public class Signin extends AppCompatActivity {
                 });
             }
         });
-
-
     }
-
 
 }
