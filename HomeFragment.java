228ebba6a2e553b470745fed4efafb297f474d6e
Viiuska <87257685+Viiuska@users.noreply.github.com>
2021-07-31 01:25:31 +0300

    Update HomeFragment.java

--- HomeFragment.java
+++ HomeFragment.java
@@ -5,9 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,62 +18,112 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
-public class ProfileFragment extends Fragment implements View.OnClickListener {
+public class HomeFragment extends Fragment implements View.OnClickListener{
 
     String person;
-    String filename = ".ProfileInfo.csv";
-    String name = ".newProf.csv";
 
-    Fragment fragment;
-    Button edit;
-    TextView userProfile, userAge, userWeight, userHeight;
+    String Food = ".FoodCounter.csv";
+    String Water = ".WaterCounter.csv";
+    String Exercise = ".Exercise.csv";
+    String name = ".newProf.csv";
+    String date;
+    TextView userNameHello, textFood, textWater, textSport, textPeriod;
 
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
 
+    //TODO pitäs siis tervehtiä käyttäjä-oliota
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        TextView dateTextHead = (TextView) rootView.findViewById(R.id.dateTextHead);
 
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
         person = fAuth.getCurrentUser().getUid();
 
-        String [] userinfo = readFile(filename, person);
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance(DateFormat.DATE_FIELD).format(calendar.getTime());
+        SimpleDateFormat format = new SimpleDateFormat("d.M.yyyy");
+        date = format.format(Date.parse(currentDate));
+        dateTextHead.setText(date);
 
-        userAge = (TextView) rootView.findViewById(R.id.userAge);
-        userWeight = (TextView) rootView.findViewById(R.id.userWeight);
-        userHeight = (TextView) rootView.findViewById(R.id.userHeight);
-        userProfile = (TextView) rootView.findViewById(R.id.userProfile);
+        userNameHello = (TextView) rootView.findViewById(R.id.userNameHello);
+        textFood = (TextView) rootView.findViewById(R.id.textFood);
+        textWater = (TextView) rootView.findViewById(R.id.textWater);
+        textSport = (TextView) rootView.findViewById(R.id.textSport);
+        textPeriod = (TextView) rootView.findViewById(R.id.textPeriod);
 
-        //TODO tiedot saatava käyttäjä-oliosta
+        String[] userinfo = readFile(name, person);
+        userNameHello.setText("Hi "+userinfo[0]+"!");
 
-        userAge.setText(userinfo[0]);
-        userWeight.setText(userinfo[1]);
-        userHeight.setText(userinfo[2]);
+        setFoodText();
+        setWaterText();
+        setExerciseText();
+        textPeriod.setText("    Menstrual tracker\n    X day left");
 
-        String [] nameinfo = readFile(name, person);
+        Button addFood = (Button) rootView.findViewById(R.id.addFood);
+        Button addWater = (Button) rootView.findViewById(R.id.addWater);
+        Button addSport = (Button) rootView.findViewById(R.id.addSport);
+        Button startPeriod = (Button) rootView.findViewById(R.id.startPeriod);
+        Button statistics = (Button) rootView.findViewById(R.id.statistics);
 
-        userProfile.setText(nameinfo[0]);
-
-        Button edit = rootView.findViewById(R.id.edit);
-
-        edit.setOnClickListener(this);
+        addFood.setOnClickListener(this);
+        addWater.setOnClickListener(this);
+        addSport.setOnClickListener(this);
+        startPeriod.setOnClickListener(this);
+        statistics.setOnClickListener(this);
 
         return rootView;
     }
 
     @Override
     public void onClick(View v) {
-        fragment = new EditProfileFragment();
+        Fragment fragment = null;
+        switch (v.getId()) {
+            case R.id.addFood:
+                fragment = new FoodFragment();
+                replaceFragment(fragment);
+                break;
+            case R.id.addWater:
+                fragment = new WaterFragment();
+                replaceFragment(fragment);
+                break;
+            case R.id.addSport:
+                fragment = new SportFragment();
+                replaceFragment(fragment);
+                break;
+            case R.id.startPeriod:
+                fragment = new PeriodFragment();
+                replaceFragment(fragment);
+                break;
+            case R.id.statistics:
+                fragment = new StatisticsFragment();
+                replaceFragment(fragment);
+                break;
+        }
+    }
+
+    //This method change page what user see
+
+    public void replaceFragment(Fragment someFragment) {
         FragmentTransaction transition = getFragmentManager().beginTransaction();
-        transition.replace(R.id.fragment_container, fragment);
+        transition.replace(R.id.fragment_container, someFragment);
         transition.addToBackStack(null);
         transition.commit();
     }
 
+    //This method read smaller files, like Water counter and food counter.
+    //It returns total from last row.
     public String[] readFile(String name,String person) {
         BufferedReader br = null;
         try {
@@ -91,8 +139,8 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
             lines = result.split(",");
 
             String wanted = lines[lines.length-1];
-            String[] userinfo = wanted.split(";");
-            return userinfo;
+            String[] info = wanted.split(";");
+            return info;
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -102,8 +150,56 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
                 ex.printStackTrace();
             }
         }
-        String[] userinfo = null;
-        return userinfo;
+        String[] info = null;
+        return info;
     }
 
+    //This method set to food counter info-box and check if there is old result to show
+    public void setFoodText(){
+        String[] foodInfo = readFile(Food,person);
+
+        if (foodInfo != null) {
+            textFood.setText("    Food counter\n    " + foodInfo[1] + " Kg CO2 last time");
+        }
+        else{
+            textFood.setText("    Food counter\n    0 Kg CO2 last time");
+        }
+    }
+
+    //This method set to water counter info-box and check if there is result from that day
+    public void setWaterText(){
+        String[] waterInfo = readFile(Water,person);
+
+        if (waterInfo != null) {
+            String oldDate = waterInfo[0];
+            if (oldDate.equals(date)){
+                textWater.setText("    Water counter\n    " + waterInfo[1] + "ml today");
+            }
+            else{
+                textWater.setText("    Water counter\n    0 ml today");;
+            }
+        }
+        else {
+            textWater.setText("    Water counter\n    0 ml today");
+        }
+    }
+
+    //This method set to exercise info-box and check if there is result from that day
+    public void setExerciseText(){
+        String[] exerciseInfo = readFile(Exercise,person);
+
+        if (exerciseInfo != null) {
+            String oldDate = (exerciseInfo[0]).trim();
+            System.out.println(oldDate);
+            if (oldDate.equals(date)){
+                textSport.setText("    Exercise\n    " + exerciseInfo[1] + ": " + exerciseInfo[2] + "h today");
+            }
+            else {
+                textSport.setText("    Exercise\n    Type: 0.00h today");
+            }
+        }
+        else {
+            textSport.setText("    Exercise\n    Type: 0.00h today");
+        }
+    }
 }
